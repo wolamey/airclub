eb98c03a32361fae947536c32c9978f7515aa7b9
надежда соломенникова <salamenikava77@gmail.com>
2025-06-17 14:27:13 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter >
+    <HashRouter  basename="/airclub">
       <App />
     </HashRouter>
   </StrictMode>
