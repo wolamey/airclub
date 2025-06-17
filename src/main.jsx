34bf8b96989a6b97bcc8b9297a27386f0d50c2b0
надежда соломенникова <salamenikava77@gmail.com>
2025-06-17 14:21:57 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App/App";
-import { BrowserRouter as Router } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router  basename="/airclub">
+    <HashRouter >
       <App />
-    </Router>
+    </HashRouter>
   </StrictMode>
 );
