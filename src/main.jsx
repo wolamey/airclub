eb2bfcf89454ff94aaa21de46d514300c1be4156
надежда соломенникова <salamenikava77@gmail.com>
2025-06-18 13:23:34 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App/App";
-import {HashRouter} from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
