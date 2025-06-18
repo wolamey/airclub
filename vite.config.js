53d9c2ae106312fac8c5bdc914fcc10608744b9c
Vladislav Sobolev <113209652+xrainie@users.noreply.github.com>
2025-06-18 11:36:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:'/airclub/',
   plugins: [react()],
+  server: {
+    allowedHosts: ['hotdesk-client.aeroclub.ru'],
+  }
 })
